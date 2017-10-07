418aef3cd10abe74d210c0af0e0c39a6b4d6fe25
Alexander <alesander1996@gmail.com>
2017-10-07 18:45:56 +0300

    migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-var CrowdSale = artifacts.require("./CrowdSale.sol");
+var CrowdSale = artifacts.require("CrowdSale");
 
-module.exports = function(deployer) {
-  deployer.deploy(CrowdSale);
+module.exports = function(deployer, network) {
+  console.log(network);
+  deployer.deploy(CrowdSale,
+  	Date.now(), 
+  	3600 * 5, 
+  	"0x685adf7de7a0ee341b8da2bdbdf26b02795f9d91",
+  	[13, 30, 50],
+  	1000*1000*1000*1000*1000*1000,
+  );
 };
