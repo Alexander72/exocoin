709dbccfb5a41dbdf83a7d88f0b21a954a77980d
Alexander <alesander1996@gmail.com>
2017-10-08 22:53:11 +0300

    fix microtime to time

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var CrowdSale = artifacts.require("CrowdSale");
 module.exports = function(deployer, network) {
   console.log(network);
   deployer.deploy(CrowdSale,
-  	Date.now(), 
+  	Math.floor(Date.now() / 1000), 
   	3600 * 5, 
   	"0x685adf7de7a0ee341b8da2bdbdf26b02795f9d91",
   	[13, 30, 50],
