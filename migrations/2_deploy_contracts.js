163de26eb52085cf3ae85c91b7aa42b420e60b87
Alexander <alesander1996@gmail.com>
2017-10-11 01:47:15 +0300

    fix deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,17 @@
+const Web3 = require('web3')
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 var CrowdSale = artifacts.require("CrowdSale");
 
-module.exports = function(deployer, network) {
-  console.log(network);
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(CrowdSale,
   	Math.floor(Date.now() / 1000), 
   	3600 * 5, 
-  	"0x685adf7de7a0ee341b8da2bdbdf26b02795f9d91",
-  	[13, 30, 50],
-  	1000*1000*1000*1000*1000*1000,
+  	accounts[2],
+  	[
+  		13,
+  		30,
+  		50,
+  	],
+  	web3.utils.toWei(1, 'ether'),
   );
 };
