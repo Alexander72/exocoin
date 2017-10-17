8c40651f01af78289ca216e7e72f94955fee37c9
Alexander <alesander1996@gmail.com>
2017-10-18 02:31:48 +0300

    front

--- app/js/app.js
+++ app/js/app.js
@@ -8,19 +8,59 @@ import CrowdSale from '../../build/contracts/CrowdSale.json'
 const TESTRPC_HOST = 'localhost'
 const TESTRPC_PORT = '8545'
 
+let crowdSaleInstance;
+let crowdSale;
+let web3;
+
+function update_time_remained() {
+
+  crowdSale.deployed()
+    .then((instance) => { 
+      return instance.finishAt.call();
+    }).then((finishAt) => {
+      let now = new Date();
+      let finish = new Date(finishAt * 1000);
+      let diff = (finish - now);
+
+      let days = Math.floor(diff / 1000 / 3600 / 24);
+      let hours = Math.floor((diff - (days * 24 * 3600 * 1000)) / 3600 / 1000);
+      let minutes = Math.floor((diff - (days * 24 * 3600 * 1000) - (hours *  3600 * 1000)) / 60 / 1000);
+      let sec = Math.floor((diff - (days * 24 * 3600 * 1000) - (hours *  3600 * 1000) - (minutes *  60 * 1000)) / 1000);
+      $("#time_remained").html(days + ' дней, ' + hours + ' часов, ' + minutes + ' минут, ' + sec + ' секунд');
+    });
+
+  setTimeout(update_time_remained, 330);
+}
+
 function component () {
-  var $element = $('<div>')
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider(`http://${TESTRPC_HOST}:${TESTRPC_PORT}`));
+  }
+
   let provider = new Web3.providers.HttpProvider(`http://${TESTRPC_HOST}:${TESTRPC_PORT}`)
-  let crowdSale = contract(CrowdSale)
+  crowdSale = contract(CrowdSale)
   crowdSale.setProvider(provider)
   crowdSale.deployed()
     .then((instance) => { 
-    	$element.html(`Metacoin address: ${instance.address}`)
-    })
-
-  return $element
+      crowdSaleInstance = instance;
+    	$("#contract_address").html(instance.address)
+      update_time_remained();
+      return crowdSaleInstance.currentStageIndex.call();
+    }).then((currentStageIndex) => {
+      return crowdSaleInstance.stageGoal.call(currentStageIndex);
+    }).then((goal) => {
+      let currGoal = web3._extend.utils.fromWei(goal, "ether").round(2).toString()
+      $("#current_stage_goal").html(currGoal);
+      return crowdSaleInstance.totalInvested.call();
+    }).then((totalInvested) => {
+      let totalInvestedFormatted = web3._extend.utils.fromWei(totalInvested, "ether").round(2).toString()
+      $("#ether_got").html(totalInvestedFormatted);
+    });
 }
 
 $(function(){
-	$('body').html(component());
+	component();
 });
