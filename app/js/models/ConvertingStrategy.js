5db5a0b6f24b525a62d20a8055237ae42fd4a8b3
Alex <iskander@start2play.ru>
2017-10-20 20:33:29 +0300

    models

--- app/js/models/ConvertingStrategy.js
+++ app/js/models/ConvertingStrategy.js
@@ -1,12 +1,12 @@
 import { default as web3Provider } from './web3.js'
 import { default as contract } from 'truffle-contract'
-import CrowdSaleABI from '../../../build/contracts/CrowdSale.json'
+import ConvertingStrategyABI from '../../../build/contracts/ConvertingStrategy.json'
 
 export default Backbone.Model.extend({
 	
 	initialize: function() {
 		this.web3 = new web3Provider();
-		this.contract = contract(CrowdSaleABI);
+		this.contract = contract(ConvertingStrategyABI);
 		this.contract.setProvider(this.web3.provider);
 	}
 
--- app/js/models/ExoCount.js
+++ app/js/models/ExoCount.js
@@ -1,12 +1,12 @@
 import { default as web3Provider } from './web3.js'
 import { default as contract } from 'truffle-contract'
-import CrowdSaleABI from '../../../build/contracts/CrowdSale.json'
+import ExoCoinABI from '../../../build/contracts/ExoCoin.json'
 
 export default Backbone.Model.extend({
 	
 	initialize: function() {
 		this.web3 = new web3Provider();
-		this.contract = contract(CrowdSaleABI);
+		this.contract = contract(ExoCoinABI);
 		this.contract.setProvider(this.web3.provider);
 	}
 
